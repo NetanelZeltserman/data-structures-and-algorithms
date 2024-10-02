9bf728c5427832cd2c1782d5199429f66cf5d8e7
NatiNati <121944905+NetanelZeltserman@users.noreply.github.com>
2024-10-02 14:44:13 +0300

    Optimize FizzBuzz Leetcode question #412

--- algorithms-practice/412_fizz_buzz.py
+++ algorithms-practice/412_fizz_buzz.py
@@ -15,14 +15,14 @@ class Solution:
         result = list()
 
         for i in range(1, n+1):
-            if (((i % 3) == 0) and (i % 5) == 0):
-                result.append("FizzBuzz")
-            elif (i % 5) == 0:
-                result.append("Buzz")
-            elif (i % 3) == 0:
-                result.append("Fizz")
-            else:
-                result.append(f"{i}")
+            r_str = ""
+            if (i % 3) == 0:
+                r_str += "Fizz"
+            if (i % 5) == 0:
+                r_str += "Buzz"
+            if not r_str:
+                r_str = str(i)
+            result.append(r_str)
 
         return result
 
